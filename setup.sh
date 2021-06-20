eb4f0a8b25266c06faab958005511cae5d2f6854
Matt Moeller <ultamatt@gmail.com>
2021-06-20 00:04:01 +0000

    Adding new stuff1

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/bash
 
-apt install -y git rsync
+apt install -y git rsync curl build-essential pkg-config nodejs npm
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env
+
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+cargo install cargo-generate
+
